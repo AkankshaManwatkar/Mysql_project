b5f16359442df4cc39cab00a4baa62edb74203a0
AkankshaManwatkar <akanksharm2001@gmail.com>
2024-06-19 11:43:44 +0530

    Update Mysql_project.sql

--- Mysql_project.sql
+++ Mysql_project.sql
@@ -18,7 +18,6 @@ select * from product_size;
 	select * from product_size
 	where sale_price > regular_price;
 
-
 4) Identify the paintings whose asking price is less than 50% of its regular price
 	select * 
 	from product_size
